d1e78892f489f6a3f1ff4fffc99b3282d1898912
Rafael <rafgonz1222@gmail.com>
2021-01-28 09:56:14 -0600

    changed text

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,14 +1,17 @@
-let f1;
+let f1,f2;
 
 function setup() {
   createCanvas(500, 500);
   textAlign(CENTER);
   f1 = loadFont("assets/bitmap1.TTF");
+  f2 = loadFont("assets/bitmap2.otf");
 }
 
 function draw() {
   background(100);
-  textSize(48);
+  textSize(24);
   textFont(f1);
-  text("hello", width/2, height/2);
+  text("scott pilgrim", width/2, height/2);
+  textFont(f2);
+  text("vs the world", width/2, height/2 + 100);
 }
